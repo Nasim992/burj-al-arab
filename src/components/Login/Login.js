e645f33ff715308a2989a4765fe5bfde71caded4
Nasim992 <mdnasim6416@gmail.com>
2020-09-29 12:12:43 +0600

    firebase-authentication

---
+++ src/components/Login/Login.js
@@ -0,0 +1,56 @@
+import React, { useContext } from 'react';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+import {UserContext} from '../../App';
+import { useHistory, useLocation } from 'react-router-dom';
+const Login = () => {
+
+    const [loggedInUser,setLoggedInUser] = useContext(UserContext);
+
+    const history = useHistory();
+    const location = useLocation();
+    const { from } = location.state || { from: { pathname: "/" } };
+
+    if(firebase.apps.length === 0 ) {
+        firebase.initializeApp(firebaseConfig);
+    }
+   
+
+
+    const handleSignIn = ()=> {
+
+        const  provider = new firebase.auth.GoogleAuthProvider();
+
+        firebase.auth().signInWithPopup(provider)
+        .then(function(result) {
+    
+            const {displayName,email} = result.user;
+            const signInUser = {name:displayName,email};
+        
+            var user = result.user;
+  
+            setLoggedInUser(signInUser);
+            history.replace(from);
+     
+          })
+          .catch(function(error) {
+   
+            var errorCode = error.code;
+            var errorMessage = error.message;
+      
+            var email = error.email;
+     
+            var credential = error.credential;
+       
+          });
+    }
+    return (
+        <div>
+            <h1>This is Login</h1>
+            <button onClick={handleSignIn}>Google Sign in</button>
+        </div>
+    );
+};
+
+export default Login;
